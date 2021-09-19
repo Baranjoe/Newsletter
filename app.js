4b63a1377846c1274f13e44524a455b805a91606
Barandun <j.barandun@lincolninternational.ch>
2021-09-19 15:13:26 +0200

    Changes

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", function(req, res) {
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  console.log(req.body.email);
+});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
