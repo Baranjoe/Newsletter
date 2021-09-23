3b62d02d4203f67d5d2a94fecc22e9c13a803921
Barandun <j.barandun@lincolninternational.ch>
2021-09-23 10:41:22 +0200

    Many Changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -8,9 +9,41 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/", function(req, res) {
-  console.log(req.body.firstName);
-  console.log(req.body.lastName);
-  console.log(req.body.email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscrubed"
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us5.api.mailchimp.com/3.0/lists/f2c7a4781ac605a3d328bfb67cae5da6-us5";
+
+  const options = {
+    method: "POST",
+    auth: "Baranjoe:f2c7a4781ac605a3d328bfb67cae5da6-us5"
+  }
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+  console.log(firstName, lastName, email);
 });
 
 app.get("/", function(req, res) {
@@ -20,3 +53,9 @@ app.get("/", function(req, res) {
 app.listen(3000, function() {
   console.log("server is running on port 3000");
 });
+
+// API KEY
+// f2c7a4781ac605a3d328bfb67cae5da6-us5
+
+// AUDIENCE ID
+// a95f1160a6
